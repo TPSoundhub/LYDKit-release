ae0ca7fc1f138c11784751a9eb6253c88f04c692
TPSoundhub <44570387+TPSoundhub@users.noreply.github.com>
2022-04-26 11:38:09 +0200

    Delete SPOR 1/LKS1/Ma directory

--- SPOR 1/LKS1/Ma/LKS1Ma-Slide18-Accelerometer.py
+++
@@ -1,9 +0,0 @@
-# Udlæsning af accelerometerværdier i x-aksen.
-from microbit import *
-
-while True:
-    acc_x,acc_y,acc_z = accelerometer.get_values()
-#    acc_all = accelerometer.get_values()          # alternativ - tuple/sæt af værdier
-    print(acc_x)                                   
-#    print(acc_all[0])                             # alternativ - udpeg i tuple
-    sleep(100)
