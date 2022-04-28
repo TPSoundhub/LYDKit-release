21b79f0abac6ab1a596358db24ac31719b0e7633
TPSoundhub <44570387+TPSoundhub@users.noreply.github.com>
2022-04-28 09:49:19 +0200

    Delete SPOR1 directory

--- SPOR1/LKS1/Ma/LKS1Ma-Slide20-Microphone.py
+++
@@ -1,7 +0,0 @@
-# Udlæsning af lyd niveau fra indbygget mikrofon.
-from microbit import *
-
-while True:
-    s = microphone.sound_level()
-    if s>0: print(s)                                   
-    sleep(10)
